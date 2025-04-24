93c74f3cee2401cfbe92ed8bdda6d8161251b1f9
champion <jphilippe.champion@gmail.com>
2025-04-24 21:58:02 +0200

    npm run server

--- App/server.js
+++ App/server.js
@@ -1,18 +1,6 @@
-
 const express = require('express');
-const connectDB = require('./config/db');
-const dotenv = require("dotenv").config();
-const port = 5000
+const port = 5000;
 
-//connexion à la base de données//
-connectDB();
 const app = express();
 
-//middleware qui traite les données de request//
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-
-app.use("/post", require('./routes/post.routes'));
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`le serveur a démarrer au port ${port}`));
