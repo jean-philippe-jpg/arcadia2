6c4380a9fac317cb3a461ce67ff4d1aaf354211f
champion <jphilippe.champion@gmail.com>
2025-06-06 19:38:53 +0200

    bug mongo atlas

--- App/mongo/config/db.js
+++ App/mongo/config/db.js
@@ -1,20 +1,25 @@
 
 const mongoose = require('mongoose');
 //const dotenv = require('dotenv').config();
+const dotenv  = require('dotenv');
+dotenv.config();
 
 
 
 
-const connectDB = async () => {
+
+
+
+/*const connectDB = async () => {
 
 try {
-     mongoose.set('strictQuery', false); 
+   await  mongoose.set('strictQuery', false); 
        
    /*mongoose.connect('mongodb+srv://arcadia:ORJ4KUzjgSAip0ub@cluster0.fgnbb.mongodb.net', ()=> 
         console.log("MongoDB connected")
-    );*/
+    );
 
-    mongoose.connect('mongodb://localhost:27017' )
+    mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected')
     );
 
@@ -23,7 +28,25 @@ try {
     process.exit(1); // Stop the app if there is an error
 }
 
+}*/
+
+async function connectDB() {
+    await mongoose.connect('mongodb+srv://jphilippechampion:jphilippechampion@cluster0.5ayfjly.mongodb.net/arcadia_db?retryWrites=true&w=majority&appName=Cluster0',{
+       //await mongoose.connect(process.env.MONGO_URI, {
+        //useNewUrlParser: true,
+        //useUnifiedTopology: true,
+       //useCreateIndex: true
+    });
+
+    console.log('MongoDB connected');
 }
+    
+
+connectDB().catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Stop the app if there is an error
+});
+
 
 module.exports = connectDB;
 
